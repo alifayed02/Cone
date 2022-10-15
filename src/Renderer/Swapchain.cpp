0342cf0ac3f39dd260dfba83a0d0b254c65b20d7
aligraphics <alifayed.h@gmail.com>
2022-10-15 00:09:00 -0700

    Swapchain Abstraction

--- src/Renderer/Swapchain.cpp
+++ src/Renderer/Swapchain.cpp
@@ -1,1 +1,43 @@
 #include "Swapchain.hpp"
+
+#include "Context.hpp"
+
+Swapchain::Swapchain(Context* context)
+        :   m_Context{context}, m_Swapchain{},
+            m_SwapchainImageFormat{}
+{
+    Init();
+}
+
+void Swapchain::Init()
+{
+    vkb::SwapchainBuilder swapchainBuilder{m_Context->GetPhysicalDevice(), m_Context->GetLogicalDevice(), m_Context->GetSurface()};
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+            .use_default_format_selection()
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+            .set_desired_extent(m_Context->GetSurfaceExtent().width, m_Context->GetSurfaceExtent().height)
+            .build()
+            .value();
+    m_Swapchain = vkbSwapchain.swapchain;
+    m_SwapchainImageFormat = vkbSwapchain.image_format;
+    m_Images = vkbSwapchain.get_images().value();
+    m_ImageViews = vkbSwapchain.get_image_views().value();
+}
+
+Swapchain::~Swapchain()
+{
+    if(m_Swapchain)
+    {
+        vkDestroySwapchainKHR(m_Context->GetLogicalDevice(), m_Swapchain, nullptr);
+    }
+
+    for(auto imageView : m_ImageViews)
+    {
+        vkDestroyImageView(m_Context->GetLogicalDevice(), imageView, nullptr);
+    }
+
+    for(auto image : m_Images)
+    {
+        vkDestroyImage(m_Context->GetLogicalDevice(), image, nullptr);
+    }
+}
--- src/Renderer/Swapchain.hpp
+++ src/Renderer/Swapchain.hpp
@@ -1,7 +1,25 @@
 #pragma once
 
+#include "Core/CnPch.hpp"
+
+class Context;
+
 class Swapchain
 {
-    Swapchain() = default;
-    ~Swapchain() = default;
+public:
+    explicit Swapchain(Context* context);
+    ~Swapchain();
+
+    Swapchain(const Swapchain& otherSwapchain) = delete;
+    Swapchain& operator=(const Swapchain& otherSwapchain) = delete;
+public:
+    inline VkFormat GetFormat() const { return m_SwapchainImageFormat; }
+private:
+    void Init();
+private:
+    Context*                    m_Context;
+    VkSwapchainKHR              m_Swapchain;
+    VkFormat                    m_SwapchainImageFormat;
+    std::vector<VkImage>        m_Images;
+    std::vector<VkImageView>    m_ImageViews;
 };
